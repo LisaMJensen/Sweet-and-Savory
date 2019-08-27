c20c3c030c56478580fb180dbf4b5be1ac21735a
Lisa Jensen <leese_38@yahoo.com>
2019-08-26 22:18:59 -0700

    add new migration and database update

--- Bakery/Migrations/20190827051707_3rdMigration.Designer.cs
+++ Bakery/Migrations/20190827051707_3rdMigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Bakery.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bakery.Migrations
 {
     [DbContext(typeof(BakeryContext))]
-    partial class BakeryContextModelSnapshot : ModelSnapshot
+    [Migration("20190827051707_3rdMigration")]
+    partial class _3rdMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
